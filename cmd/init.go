1b1636144a4121872771c10da134c6fb3d87f865
andres.puello <andres.puello@rappi.com>
2022-11-05 00:00:02 -0500

    feat: Add Progress Bar
    - add new flag --go_module [opcional] true/false (defualt true)

--- cmd/init.go
+++ cmd/init.go
@@ -12,31 +12,40 @@ import (
 )
 
 var (
+	mod     []bool
 	initCmd = &cobra.Command{
 		Use:   "init",
 		Short: "Initializes the creation of the files and folders.",
 		Long: `Initializes the creation of the files and folders necessary for the 
 hexagonal structure of the microservice`,
-		Example: `gohex init --path /api/gohex --module github.com/andresxlp/gohex`,
+		Example: `gohex init --path /api/hexa --module github.com/andresxlp/hexa-ms`,
 		Run: func(cmd *cobra.Command, args []string) {
+			handler.ProgressBarFolderAndFile()
 			handler.StarCreate(&app.Service{})
 		},
 		PostRun: func(cmd *cobra.Command, args []string) {
-			handler.StartGoModule(&app.Service{})
-			fmt.Println(`Initial Hexagonal Struct Created Successfully.
+			fmt.Print(`Initial Hexagonal Struct Created Successfully.
 
 Don't forget to set the environment variables:
 SERVER_HOST
-SERVER_PORT`)
+SERVER_PORT
 
+`)
+			if mod[0] {
+				handler.ProgressBarGoModule()
+				handler.StartGoModule(&app.Service{})
+			} else {
+				fmt.Println("Don't forget execute go mod init and go mod tidy")
+			}
 		},
 	}
 )
 
 func init() {
-	initCmd.Flags().StringVarP(&handler.BasePath, "path", "p", "", "REQUIRED: set base path for router. e.g: /api/gohex")
+	initCmd.Flags().StringVarP(&handler.BasePath, "path", "p", "", "REQUIRED: set base path for router. e.g: /api/hexa")
 	initCmd.MarkFlagRequired("path")
-	initCmd.Flags().StringVarP(&handler.Module, "module", "m", "", "REQUIRED: set your Module Go. e.g: github.com/andresxlp/gohex")
-	initCmd.MarkFlagRequired("module")
+	initCmd.Flags().StringVarP(&handler.Module, "import", "i", "", "REQUIRED: set your Package Module in the imports. e.g: github.com/andresxlp/hexa-ms")
+	initCmd.MarkFlagRequired("import")
+	initCmd.Flags().BoolSliceVar(&mod, "go_module", []bool{true}, "[opcional] true / false - Run go mod init & go mod tidy.")
 	rootCmd.AddCommand(initCmd)
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,19 @@ module github.com/andresxlp/gohex
 
 go 1.18
 
-require github.com/spf13/cobra v1.6.1
+require (
+	github.com/k0kubun/go-ansi v0.0.0-20180517002512-3bf9e2903213
+	github.com/schollz/progressbar/v3 v3.12.0
+	github.com/spf13/cobra v1.6.1
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.4.2 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/term v0.1.0 // indirect
 )
